26865273440dc439ecd1621f055573d065ff8754
kimyerim1935 <kimyerim1935@naver.com>
2022-12-24 20:33:11 +0900

    add: 로컬스토리지 getItem

--- 2주차/index.js
+++ 2주차/index.js
@@ -1,23 +1,8 @@
 import App from './App.js';
+import {getItem} from "./localStorage.js";
+import {STORAGE_KEY} from "./constants.js";
 
-const initialState = [
-  {
-    text: 'JS 공부하기',
-    isCompleted: false,
-  },
-  {
-    text: 'JS 복습하기',
-    isCompleted: false,
-  },
-  {
-    text: 'TS 공부하기',
-    isCompleted: false,
-  },
-  {
-    text: 'TS 복습하기',
-    isCompleted: false,
-  },
-];
+const initialState = getItem(STORAGE_KEY, [])
 
 const $target = document.querySelector('main');
 
