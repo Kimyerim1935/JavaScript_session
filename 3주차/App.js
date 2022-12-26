03ca64669a003a7933ec0254bfa5139ff2da39b6
kimyerim1935 <kimyerim1935@naver.com>
2022-12-26 22:20:15 +0900

    add: 출연 뮤지션 list 추가 및 클릭 시 검색

--- 3주차/App.js
+++ 3주차/App.js
@@ -1,7 +1,10 @@
 import SearchInput from "./SearchInput.js";
+import SearchHistory from "./SearchHistory.js";
 import SearchResult from './SearchResult.js';
 import {request} from './api.js';
 import debounce from "./debounce.js";
+
+const MAX_HISTORY_COUNT = 5;
 
 export default function App({$target, initialState}) {
   this.state = initialState;
@@ -9,30 +12,67 @@ export default function App({$target, initialState}) {
   this.setState = (nextState) => {
     this.state = nextState;
 
-    searchResult.setState(this.state);
+    searchResult.setState(this.state.results);
+    searchHistory.setState(this.state.histories);
   }
 
   const searchInput = new SearchInput({
     $target,
-    initialState: this.state,
+    initialState: '',
     handleSearch: debounce(async (keyword) => {
       try {
         if (keyword.length > 0) {
           const results = await request(`/api/search?keyword=${keyword}`);
 
-          this.setState(results)
+          let nextHistories = [...this.state.histories];
+
+          if (nextHistories.includes(keyword)) {
+            nextHistories = [
+              ...nextHistories.filter((history) => history !== keyword),
+              keyword,
+            ];
+          } else {
+            nextHistories = [...nextHistories, keyword];
+          }
+          if (nextHistories.length > MAX_HISTORY_COUNT) {
+            nextHistories = nextHistories.slice(
+              nextHistories.length - MAX_HISTORY_COUNT,
+              MAX_HISTORY_COUNT + 1
+            );
+          }
+          this.setState({
+            results,
+            histories: nextHistories,
+          });
         }
       } catch (e) {
         alert('오류가 발생했습니다')
       }
-    })}
-  )
+    }, 300)
+  })
 
-  const searchResult = new SearchResult({$target, initialState: this.state})
+  const searchHistory = new SearchHistory({
+    $target,
+    initialState: []
+  })
 
-  this.render = () => {
+  const searchResult = new SearchResult({
+    $target,
+    initialState: this.state.results,
+    handleMusicianClick: async (musician) => {
+      try {
+        const results = await request(`/api/search?keyword=${musician}`)
+        this.setState({
+          results,
+          histories: [...this.state.histories]
+        })
 
-  }
+        searchInput.setState(musician);
+      } catch (e) {
+        alert('에러가 발생했습니다..')
+      }
 
-  this.render();
+    }
+  })
+
 }
--- 3주차/SearchResult.js
+++ 3주차/SearchResult.js
@@ -1,4 +1,4 @@
-export default function SearchResult({$target, initialState}) {
+export default function SearchResult({$target, initialState, handleMusicianClick}) {
   this.state = initialState;
 
   this.$element = document.createElement('div');
@@ -13,12 +13,27 @@ export default function SearchResult({$target, initialState}) {
   this.render = () => {
     const htmlString = `${this?.state
       .map(
-      (d) =>
-        `<div style="display: inline-block; width: 33%"><img src="${d.poster}" style="object-fit: cover; width: 100%;"></div>`
-    )
-      .join('')}`
+        (d) =>
+          `<div style="display: inline-block; width: 33%">
+            <img src="${d.poster}" style="object-fit: cover; width: 100%;">
+             ${d.musicians
+            .map(
+              (musician) =>
+                `<p class="musician" style="display: inline-block; padding: 4px; border: 1px solid #ccc; margin-right: 4px;">${musician}</p>`
+            )
+            .join('')}
+          </div>`
+      )
+      .join('')}`;
 
     this.$element.innerHTML = htmlString;
   }
+
+  this.$element.addEventListener('click', (e) => {
+    if (e.target.className === 'musician') {
+      handleMusicianClick(e.target.textContent);
+    }
+  });
+
 
 }
