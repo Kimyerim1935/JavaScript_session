f0e9d9c85f25bceac8d25205ce6f21c527caa71b
kimyerim1935 <kimyerim1935@naver.com>
2022-12-26 22:23:12 +0900

    add: index initialState 정의

--- 3주차/index.js
+++ 3주차/index.js
@@ -2,4 +2,8 @@ import App from './App.js';
 
 const $target = document.querySelector('main');
 
-new App({$target, initialState: []})
+new App({$target, initialState: {
+    results: [],
+    histories: [],
+    keyword: '',
+  }})
